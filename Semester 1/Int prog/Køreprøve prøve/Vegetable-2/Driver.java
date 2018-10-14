0ef58464ddb5f710cd5d92171eea4b4746ec45d7
Nimakii <jens_kristian152@hotmail.com>
2018-10-14 19:07:28 +0200

    141018-s

---
+++ Semester 1/Int prog/Køreprøve prøve/Vegetable-2/Driver.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class was here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Driver
+{
+    public static void exam(){
+        Vegetable v1 = new Vegetable("carrots",true,200);
+        Vegetable v2 = new Vegetable("carrots",false,50);
+        Vegetable v3 = new Vegetable("tatoes",false,200);
+        Vegetable v4 = new Vegetable("tatoes",true,75);
+        Vegetable v5 = new Vegetable("greens",false,250);
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+        System.out.println(v4);
+        System.out.println(v5);
+        Shop myShop = new Shop("JK");
+        myShop.add(v1);
+        myShop.add(v2);
+        myShop.add(v3);
+        myShop.add(v4);
+        myShop.add(v5);
+        System.out.println();
+        System.out.println("Imperative");
+        System.out.println(myShop.findI(false));
+        System.out.println(myShop.organicI());
+        myShop.printShopI();
+        System.out.println();
+        System.out.println("Functional");
+        System.out.println(myShop.findF(false));
+        System.out.println(myShop.organicF());
+        myShop.printShopF();
+    }
+}
---
+++ Semester 1/Int prog/Køreprøve prøve/Vegetable-2/Shop.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.util.stream.Collectors;
+/**
+ * Write a description of class wasd here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Shop
+{
+    private String owner;
+    private List<Vegetable> vegs;
+    
+    public Shop(String owner){
+        this.owner=owner;
+        this.vegs = new ArrayList<Vegetable>();
+    }
+    public void add(Vegetable v){
+        vegs.add(v);
+    }
+    
+    public Vegetable findI(boolean organic){
+        Vegetable res = null;
+        for (Vegetable v : vegs){
+            if (v.getOrganic() == organic){
+               res = v; 
+            }
+        }
+        return res;
+    }
+    public Optional<Vegetable> findF(boolean organic){
+        return vegs.stream().filter(v -> v.getOrganic() == organic)
+                            .min(Comparator.comparing(v -> v.getName()));
+    }
+    
+    public boolean organicI(){
+        int n = 0;
+        int m = 0;
+        for (Vegetable v : vegs){
+            if(v.getOrganic()){
+                n += v.getNumber();
+            }
+            else{
+                m += v.getNumber();
+            }
+        }
+        return (n>m)?true:false;
+    }
+    public boolean organicF(){
+        int n = vegs.stream().filter(v->v.getOrganic()).mapToInt(v->v.getNumber()).sum();
+        int m = vegs.stream().filter(v->!v.getOrganic()).mapToInt(v->v.getNumber()).sum();
+        
+        return (n>m)?true:false;
+    }
+    
+    public void printShopI(){
+        System.out.println(owner);
+        Collections.sort(vegs);
+        for(Vegetable v : vegs){
+            System.out.println(v);
+        }
+        Collections.shuffle(vegs);
+    }
+    public void printShopF(){
+        System.out.println(owner);
+        Collections.sort(vegs , Comparator.comparing((Vegetable v) -> v.getName())
+                                          .thenComparing(v -> -v.getNumber()));
+        vegs.forEach(v -> System.out.println(v));
+        Collections.shuffle(vegs);
+    }
+    
+    public int cheapest(boolean electric){
+        Tool res = null;
+        for(Tool t : tb){
+            if(t.getE() == electric && (t.getPrice()<res.getPrice() || res == null){
+                res = t;
+            }
+        }
+        return t.getPrice();
+    }
+}
