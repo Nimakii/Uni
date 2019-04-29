f81ccf52264dd903839c1dc9ce15e2d32160f27a
Nimakii <jens_kristian152@hotmail.com>
2019-04-29 14:23:20 +0200

    290419
    lappy1

---
+++ Semester 2/progsprog/MiniScala6/src/miniscala/Exercise122.scala
@@ -0,0 +1,204 @@
+package miniscala
+
+object Exercise122 {
+
+  def main(args: Array[String]) = {
+
+    /**
+      * Lists.
+      */
+    sealed abstract class List[+T] {
+
+      def filter(p: T => Boolean): List[T] = this match {
+        case Nil => Nil
+        case Cons(y, ys) =>
+          val r = ys.filter(p)
+          if (p(y)) Cons(y, r) else r
+      }
+
+      def map[U](f: T => U): List[U] = this match {
+        case Nil => Nil
+        case Cons(y, ys) => Cons(f(y), ys.map(f))
+      }
+
+      def foldRight[B](z: B, f: (T, B) => B): B = this match {
+        case Nil => z
+        case Cons(h, t) => f(h, t.foldRight(z, f))
+      }
+    }
+
+    case object Nil extends List[Nothing]
+
+    case class Cons[T](x: T, xs: List[T]) extends List[T]
+
+
+    /**
+      * Streams.
+      */
+    sealed abstract class Stream[+T] {
+
+      override def toString(): String = this match {
+        case SCons(x, xs) => x().toString + ", " + xs().toString()
+        case SNil => ""
+      }
+
+      def head(): T = this match {
+        case SNil => throw new RuntimeException("stream is empty")
+        case SCons(x, _) => x()
+      }
+
+      def tail(): Stream[T] = this match {
+        case SNil => throw new RuntimeException("stream is empty")
+        case SCons(_, xs) => xs()
+      }
+
+      def map[U](f: T => U): Stream[U] = this match {
+        case SNil => SNil
+        case SCons(x, xs) => SCons(() => f(x()), () => xs().map(f))
+      }
+
+      def foreach(f: T => Unit): Unit = this match {
+        case SNil =>
+        case SCons(x, xs) =>
+          f(x())
+          xs().foreach(f)
+      }
+
+      def filter(p: T => Boolean): Stream[T] = this match {
+        case SNil => SNil
+        case SCons(y, ys) =>
+          //val f = () => if (p(y)) y else Nil
+          //SCons(() => f,() => ys().filter(p))
+          if (p(y())) SCons(y, () => ys().filter(p)) else ys().filter(p)
+      }
+
+      def zip[U](ys: Stream[U]): Stream[(T, U)] = (this, ys) match {
+        case (SNil, SNil) => SNil
+        case (SNil, SCons(_, _)) => SNil
+        case (SCons(_, _), SNil) => SNil
+        case (SCons(x, xs), SCons(z, zs)) => SCons(() => (x(), z()), () => xs().zip(zs()))
+      }
+
+      def take(n: Int): Stream[T] =
+        if (n == 0) SNil else SCons(() => head(), () => tail().take(n - 1))
+
+      def foldRight[B](z: () => B, f: (T, () => B) => B): B = this match {
+        case SNil => z()
+        case SCons(h, t) => f(h(), () => t().foldRight(z, f))
+      }
+
+      def toList(): List[T] = this match {
+        case SNil => Nil
+        case SCons(x, xs) => Cons(x(), xs().toList())
+      }
+
+    }
+
+    case object SNil extends Stream[Nothing]
+
+    case class SCons[T](x: () => T, xs: () => Stream[T]) extends Stream[T]
+
+
+    val s1: Stream[Int] =
+      SCons(() => 1, () =>
+        SCons(() => 2, () =>
+          SCons(() => 3, () =>
+            SNil)))
+
+    //println(s"first element of s1: ${s1.head()}")
+    //println(s"second element of s1: ${s1.tail().head()}")
+
+    def listToStream[T](xs: List[T]): Stream[T] = xs match {
+      case Nil => SNil
+      case Cons(y, ys) => SCons(() => y, () => listToStream(ys))
+    }
+
+    val ones: Stream[Int] = {
+      def gen(): Stream[Int] = SCons(() => 1, () => gen())
+
+      gen()
+    }
+
+    val nats: Stream[Int] = {
+      def gen(n: Int): Stream[Int] = SCons(() => n, () => gen(n + 1))
+
+      gen(0)
+    }
+
+    val fibs: Stream[Int] = {
+      def gen(a: Int, b: Int): Stream[Int] = SCons(() => a, () => gen(a + b, a))
+
+      gen(0, 1)
+    }
+
+    //println("Fibonacci:")
+    //fibs.take(25).foreach(println)
+
+    def sieve(xs: Stream[Int]): Stream[Int] =
+      SCons(() => xs.head(), () => sieve(xs.tail().filter(x => x % xs.head() != 0)))
+
+    val primes = sieve(nats.tail().tail())
+
+    //println("Primes:")
+    //primes.take(100).foreach(println)
+
+    def fibs2(): Stream[Int] =
+      SCons(() => 0, () =>
+        SCons(() => 1, () =>
+          fibs2().zip(fibs2().tail()).map(n => n._1 + n._2)))
+
+    //println("Fibonacci again:")
+    //fibs2().take(25).foreach(println)
+
+    //println(s"Fibonacci as list: ${fibs2().take(25).toList()}")
+
+    class Sighting(
+                    val animal: String, // Which animal
+                    val spotter: Int, // Who saw it
+                    val count: Int, // How many
+                    val area: Int, // Where
+                    val period: Int // When
+                  )
+
+    val sightings: List[Sighting] =
+      Cons(new Sighting("Elephant", 17, 5, 3, 1),
+        Cons(new Sighting("Lion", 2, 5, 3, 2),
+          Cons(new Sighting("Elephant", 2, 3, 3, 2),
+            Nil)))
+
+    val elephants1: Int =
+      sightings.filter(s => s.animal == "Elephant")
+        .map(s => s.count)
+        .foldRight(0, (x: Int, res: Int) => x + res)
+    //println(s"Elephant sightings: $elephants1")
+
+    val elephants2: Int =
+      listToStream(sightings).filter(s => s.animal == "Elephant")
+        .map(s => s.count)
+        .foldRight(() => 0, (x: Int, res: () => Int) => x + res())
+    //println(s"Elephant sightings: $elephants2")
+
+    def f(x: Int): Int = x * x * x
+
+    val s: Stream[Int] = SCons(() => f(1), () => SCons(() => f(2), () => SCons(() => f(3), () => SNil)))
+    println(s.tail())
+    println(s.head())
+    println(s.tail().tail())
+    println(s.tail().head())
+
+
+    def unfoldRight[A, S](z: S, f: S => Option[(A, S)]): Stream[A] =
+      f(z) match {
+        case Some((h, s)) => SCons(() => h, () => unfoldRight(s, f))
+        case None => SNil
+      }
+    val onez = unfoldRight(1,(x: Int)=>Option(1,1))
+    println(onez.take(25))
+    val natz = unfoldRight(0,(x:Int)=> Option(x,x+1))
+    println(natz.take(25))
+    val g: (Int,Int) => Option[(Int,(Int,Int))] = (x,y) => Option((x,(y,x+y)))
+    val fibz = unfoldRight[Int,(Int,Int)]((0,1),x => Option((x._1,(x._2,x._1+x._2))))
+    println(fibz.take(25))
+    println(fibs2.take(25))
+  }
+}
---
+++ Semester 2/progsprog/MiniScala6/src/miniscala/a12.scala
@@ -0,0 +1,138 @@
+package miniscala
+
+import scala.util.Random
+
+object Week12 {
+  /**sealed abstract class IntList
+  case object Nil extends IntList
+  case class Cons(x: Int, xs: IntList) extends IntList
+    */
+  sealed abstract class List[+T]
+  case object Nil extends List[Nothing]
+  case class Cons[T](x: T, xs: List[T]) extends List[T]
+
+  abstract class Comparable[T] { // in real Scala code, this would be a “trait”, not an abstract class
+    /** Returns <0 if this<that, ==0 if this==that, and >0 if this>that */
+    def compareTo(that: T): Int
+  }
+
+  class Student(val id: Int) extends Comparable[Student] {
+    def compareTo(that: Student) = this.id - that.id
+    override def toString: String = s"$id"
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    println(merge(Cons(new Student(2), Cons(new Student(5), Cons(new Student(7), Nil))), Cons(new Student(3), Cons(new Student(5), Nil))))
+    println(split(Cons(new Student(2), Cons(new Student(3), Cons(new Student(5), Cons( new Student(5), Cons(new Student(7), Nil))))), 2))
+    println(ordered(Cons(new Student(2), Cons(new Student(3), Cons(new Student(5), Cons(new Student(5), Cons(new Student(7), Nil)))))))
+    println(ordered(Cons(new Student(2), Cons(new Student(3), Cons(new Student(500), Cons(new Student(5), Cons(new Student(7), Nil)))))))
+    println(reverse(Cons(new Student(3), Cons(new Student(2),Nil))))
+    println(reverse(Cons(new Student(3), Cons(new Student(5),Nil))))
+    println(randomIntList())
+    println(testMergeSort())
+    println("finished")
+    println(mergeSort(Cons(new Student(200), Cons(new Student(30), Cons(new Student(500), Cons(new Student(400), Cons(new Student(7), Nil)))))))
+  }
+
+
+
+  def merge[T <: Comparable[T]](xs: List[T], ys: List[T]): List[T] = mergee(xs,ys,Nil)
+  /**
+    * Helping function for merge method. Easier to return a list this way.
+    *
+    */
+  def mergee[T<:Comparable[T]](xs: List[T], ys: List[T], ass: List[T]):
+  List[T] = (xs,ys) match{
+    case (Nil,Nil) => reverse(ass)
+    case (Nil,Cons(z,zs)) => mergee(xs,zs,Cons(z,ass))
+    case (Cons(z,zs),Nil) => mergee(zs,ys,Cons(z,ass))
+    case (Cons(z,zs),Cons(w,ws)) =>
+      if(z.compareTo(w)<=0) mergee(zs,ys,Cons(z,ass))
+      else mergee(xs,ws,Cons(w,ass))
+  }
+
+  def split[T](xs: List[T], n: Int): (List[T], List[T]) =
+    if( length(xs)<= n)
+      (xs,Nil)
+    else if (n<0)
+      throw new RuntimeException("Illegal index")
+    else
+      splitt(xs,n,Nil)
+
+  def splitt[T](xs: List[T],n: Int, ass: List[T]) : (List[T],List[T]) = xs match{
+    case Cons(z,zs) => if (n>0) splitt(zs,n-1,Cons(z,ass)) else (reverse(ass),xs)
+
+  }
+  def ordered[T <: Comparable[T]](xs: List[T]): Boolean = xs match {
+    case Nil => true
+    case Cons(_,Nil) => true
+    case Cons(x,Cons(y,ys)) => if (x.compareTo(y)<=0) ordered(ys) else false
+  }
+  def permuted[T<:Comparable[T]](xs: List[T], ys: List[T]): Boolean =
+    if (length(xs) == length(ys)) listChecker(xs, ys) //a necessary condition
+    else false
+  def boringMerge[T<:Comparable[T]](xs: List[T],ys: List[T]): List[T] = xs match{
+    case Nil => ys //we are done
+    case Cons(z,zs) => boringMerge(zs,Cons(z,ys)) //merges xs and ys with no regard for sequence
+  }
+  def elementChecker[T <: Comparable[T]](x: T, ys: List[T], ass: List[T]):
+  (Boolean,List[T],List[T]) = ys match{
+    case Nil => (false,ys,ass) //x was not found in ys
+    case Cons(z,zs) =>
+      if (x.compareTo(z)==0) (true,zs,ass) //x was found in ys, return elements after and before x
+      else elementChecker(x,zs,Cons(z,ass)) //looks at next value in ys, with z added to accumulator
+  }
+  def listChecker[T<:Comparable[T]](xs: List[T],ys: List[T]): Boolean = xs match{
+    case Cons(x,zs) =>
+      val eC = elementChecker(x,ys,Nil) //checks if x is in y
+      if(eC._1) listChecker(zs,boringMerge(eC._2,eC._3)) //continues without x in xs and x in ys
+      else false
+    case Nil => true //xs is empty, and since the length of xs and ys are the same and we remove 1 element from each, ys is empty too
+  }
+
+  def testMergeSort(): Unit =  testMergeSortHelp(100)
+  def testMergeSortHelp(n: Int): Unit ={
+    if (n > 0){
+      val x = randomIntList()
+      val y = mergeSort(x)
+      assert(ordered(y))
+      assert(permuted(x,y))
+      testMergeSortHelp(n - 1)
+    }
+  }
+  def mergeSort[T <: Comparable [T]](xs: List[T]): List[T] = {
+    val n = length(xs) / 2
+    if (n == 0) xs
+    else {
+      val (left, right) = split(xs, n)
+      merge(mergeSort(left), mergeSort(right))
+    }
+  }
+  /**
+    * Helping functions
+    */
+  def reverse[T](xs: List[T]): List[T] = xs match {
+    case Nil => Nil
+    case Cons(x, ys) => append(reverse(ys), x)
+  }
+  def append[T](xs: List[T], x: T): List[T] = xs match {
+    case Nil => Cons(x, Nil)
+    case Cons(y, ys) => Cons(y, append(ys, x))
+  }
+  def length[T](xs: List[T]): Int = xs match {
+    case Nil => 0
+    case Cons(_, ys) => 1 + length(ys)
+  }
+
+  def randomIntList(): List[Student] = randomStudentListt(Nil,new Random().nextInt(101))
+  def randomStudentListt(ass : List[Student],n:Int): List[Student] = {
+    if(n>0) {
+      val p: Int = new Random().nextInt()
+      val s: Student = new Student(p)
+      randomStudentListt(Cons(s, ass), n - 1)
+    }
+    else
+      ass
+  }
+}
